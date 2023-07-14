ccb9a3d13cb2cc180d5ff03555300afa7925b9be
Jonmari Dandal <jonmari.dandal.solid@gmail.com>
2023-07-14 13:05:18 +0800

    import toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import { theme } from "./themeConfig/config.js";
 import { UserContextProvider } from "./context/userContext.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <ChakraProvider theme={theme}>
             <UserContextProvider>
                 <App />
+                <ToastContainer />
             </UserContextProvider>
         </ChakraProvider>
     </React.StrictMode>,
