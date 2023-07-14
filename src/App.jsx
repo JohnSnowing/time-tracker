bf693f839c1d7ec2b62efc26832f64c1c18d116d
Jonmari Dandal <jonmari.dandal.solid@gmail.com>
2023-07-14 11:55:20 +0800

    Set up routes and pages also layouts for the parent layout of authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AuthLayout from "./layout/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-    return <>test</>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/auth" element={<AuthLayout />}>
+                        <Route index element={<Login />} />
+                        <Route path="register" element={<Register />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
