76afcbc509892ccfc7c62d7bdf46370d5665df8e
Jonmari Dandal <jonmari.dandal.solid@gmail.com>
2023-07-14 12:29:45 +0800

    Layout and login design and layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,9 +1,65 @@
-import { Outlet } from "react-router-dom";
+import { Box, Container, Heading, Icon, Stack, Text } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import { RiTimerFlashFill } from "react-icons/ri";
+import { useState } from "react";
+
 const AuthLayout = () => {
+    const [parentLayoutText, setParentLayoutText] = useState({
+        headerText: "",
+        secondaryHeaderText: "",
+        linkName: "",
+        linkUrl: "",
+    });
     return (
-        <div>
-            <Outlet />
-        </div>
+        <Container
+            maxW="lg"
+            py={{ base: "12", md: "24" }}
+            px={{ base: "0", sm: "8" }}
+        >
+            <Stack spacing="8">
+                <Box
+                    py={{ base: "0", sm: "8" }}
+                    px={{ base: "4", sm: "10" }}
+                    bg={{ base: "transparent", sm: "bg.surface" }}
+                    boxShadow={{ base: "none", sm: "md" }}
+                    borderRadius={{ base: "none", sm: "xl" }}
+                    width={{ sm: "100%", md: "448px", lg: "448px" }}
+                    backgroundColor="white"
+                >
+                    <Stack
+                        spacing="6"
+                        textAlign="center"
+                        align="center"
+                        marginBottom="50px"
+                    >
+                        <Icon
+                            as={RiTimerFlashFill}
+                            fontSize="80px"
+                            color="brand.100"
+                        />
+                        <Stack
+                            spacing={{ base: "2", md: "3" }}
+                            textAlign="center"
+                        >
+                            <Heading fontSize="30px" color="textColor.h1">
+                                {parentLayoutText.headerText}
+                            </Heading>
+                            <Text color="textColor.p">
+                                {parentLayoutText.secondaryHeaderText}{" "}
+                                <Link to={parentLayoutText.linkUrl}>
+                                    {parentLayoutText.linkName}
+                                </Link>
+                            </Text>
+                        </Stack>
+                    </Stack>
+                    <Stack spacing="6">
+                        <Stack spacing="5">
+                            <Outlet context={{ setParentLayoutText }} />
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Container>
     );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    IconButton,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Stack,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+import { useOutletContext } from "react-router-dom";
 
 const Login = () => {
-    return <div>login</div>;
+    const [isOpen, setIsOpen] = useState(false);
+    const [inputs, setInputs] = useState({ email: "", password: "" });
+    const { setParentLayoutText } = useOutletContext();
+
+    const onClickReveal = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        setParentLayoutText({
+            headerText: "Log in to your account",
+            secondaryHeaderText: "Don't have an account?",
+            linkName: "Sign up",
+            linkUrl: "register",
+        });
+    }, []);
+
+    const handleChangeInput = (event) => {
+        setInputs((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <form onSubmit={handleLogin}>
+                <FormControl mb="1rem">
+                    <FormLabel htmlFor="email" color="">
+                        Email
+                    </FormLabel>
+                    <Input
+                        required
+                        type="email"
+                        id="email"
+                        name="email"
+                        _placeholder={{ color: "gray.500" }}
+                        onChange={handleChangeInput}
+                        fontSize="13pt"
+                        _hover={{
+                            bg: "white",
+                            border: "1px solid",
+                            borderColor: "brand.100",
+                        }}
+                        _focus={{
+                            outline: "none",
+                            bg: "White",
+                            border: "1px solid",
+                            borderColor: "brand.100",
+                        }}
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="email" color="">
+                        Password
+                    </FormLabel>
+                    <InputGroup>
+                        <InputRightElement>
+                            <IconButton
+                                variant="text"
+                                backgroundColor="transparent"
+                                color="textColor.label"
+                                aria-label={
+                                    isOpen ? "Mask password" : "Reveal password"
+                                }
+                                icon={isOpen ? <HiEyeOff /> : <HiEye />}
+                                onClick={onClickReveal}
+                            />
+                        </InputRightElement>
+                        <Input
+                            required
+                            name="password"
+                            type={isOpen ? "text" : "password"}
+                            onChange={handleChangeInput}
+                            autoComplete="current-password"
+                            _hover={{
+                                bg: "white",
+                                border: "1px solid",
+                                borderColor: "brand.100",
+                            }}
+                            _focus={{
+                                outline: "none",
+                                bg: "White",
+                                border: "1px solid",
+                                borderColor: "brand.100",
+                            }}
+                        />
+                    </InputGroup>
+                </FormControl>
+                <Stack spacing="6" mt="2rem">
+                    <Button variant="primary" type="submit">
+                        Sign in
+                    </Button>
+                </Stack>
+            </form>
+        </>
+    );
 };
 
 export default Login;
